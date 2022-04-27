4b7a719df6a7ee91ca502e9465e77ec8a8e90c8c
YauheniRusinchuk <ruevgal@gmail.com>
2022-04-27 14:10:07 +0300

    add handler open camera

--- src/App.js
+++ src/App.js
@@ -2,10 +2,37 @@ import React from "react";
 import { isMobile } from "react-device-detect";
 
 const MobileDevice = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onHandlerOpenCamera = () => {
+    setIsOpen(true);
+    if (navigator.webkitGetUserMedia != null) {
+      var options = {
+        video: true,
+        audio: true,
+      };
+
+      // запрашиваем доступ к веб-камере
+      navigator.webkitGetUserMedia(
+        options,
+        function(stream) {
+          // получаем тег video
+          var video = document.querySelector("video");
+          // включаем поток в магический URL
+          video.src = window.webkitURL.createObjectURL(stream);
+        },
+        function(e) {
+          console.log("error happened");
+        }
+      );
+    }
+  };
+
   return (
     <>
       <h1>Вы зашли с мобильного устройства. Так что можете открыть камеру</h1>
-      <button>Открыть камеру</button>
+      <button onClick={() => onHandlerOpenCamera()}>Открыть камеру</button>
+      {isOpen && <video width="300px" height="320px" autoplay></video>}
     </>
   );
 };
